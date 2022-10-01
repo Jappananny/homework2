e2e28f1905081d9aba91d8c3d6a234405047f24d
Jappananny <Jappananny@gmail.com>
2022-10-01 23:04:55 +0300

    ДЗ2-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        System.out.println("Задача 1");
+        int astronautsNasa = 2;
+        System.out.println("Значение переменной astronautsNasa с типом int равно " +astronautsNasa);
+        byte apolloMoonShip = 11;
+        System.out.println("Значение переменной apolloMoonShip с типом byte равно " + apolloMoonShip);
+        short inTime = 2007;
+        System.out.println("Значение переменной inTime с типом short равно " + inTime);
+        long inYear = 1969L;
+        System.out.println("Значение переменной inYear с типом long равно " + inYear);
+        float moonDistanse = 238.9F;
+        System.out.println("Значение переменной moonDistanse с типом float равно " + moonDistanse);
+        double itIsHappened = 21.07025615;
+        System.out.println("Значение переменной itIsHappened с типом double равно " + itIsHappened);
+
+
+
+
+
+
+
+
+
     }
 }
+
